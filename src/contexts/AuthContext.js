8bfe5d5331cc01fd46e4277f94c2fd46209a68ed
masa <miura.worker@gmail.com>
2022-10-30 09:47:55 +0900

    first commit

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,69 @@
+import { createContext, useContext, useState, useEffect } from 'react'
+import { auth } from '../firebase'
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  updateEmail as updateFirebaseEmail,
+  updatePassword as updateFirebasePassword,
+  signOut,
+} from 'firebase/auth'
+
+const AuthContext = createContext()
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+export default function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState()
+  const [loading, setLoading] = useState(true)
+
+  function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+
+  function logout() {
+    return signOut(auth)
+  }
+
+  function resetPassword(email) {
+    return sendPasswordResetEmail(auth, email)
+  }
+
+  function updateEmail(email) {
+    return updateFirebaseEmail(auth, email)
+  }
+
+  function updatePassword(password) {
+    return updateFirebasePassword(auth, password)
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user)
+      setLoading(false)
+    })
+    return unsubscribe
+  })
+
+  const value = {
+    currentUser,
+    signup,
+    login,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword,
+  }
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  )
+}
